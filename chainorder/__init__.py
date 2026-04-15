eb135f33ad16a57fdf4278110e067cec5810fd86
Benjamin <b.j.morgan@bath.ac.uk>
2026-04-15 18:56:16 +0100

    Scaffold chainorder package with editable install

---
+++ chainorder/__init__.py
@@ -0,0 +1,6 @@
+"""chainorder: chain decomposition and order parameters for ReO3-type anion ordering."""
+from chainorder.decompose import decompose
+from chainorder import order_params
+
+__version__ = "0.1.0"
+__all__ = ["decompose", "order_params"]
